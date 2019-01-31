aeda14f0f80ccad0e42b7b80c22ffed1f892d9b4
Tracy Ho <tracyho0867@gmail.com>
2019-01-31 19:10:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,11 @@
 class CashRegister
   attr_accessor :total, :discount
 
+
   def initialize(discount=nil)
     @total = 0
     @discount = discount
+    @items = []
   end
 
   def add_item(title, price, quantity=1)
@@ -20,6 +22,6 @@ class CashRegister
   end
 
   def items
-    
+
   end
 end
