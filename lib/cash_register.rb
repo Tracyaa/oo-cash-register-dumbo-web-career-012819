e781113c213143966cfc919a38cc38862ace7e82
Tracy Ho <tracyho0867@gmail.com>
2019-01-31 19:03:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def apply_discount # 1000 to 800
-    if @discount
+    if @discount == nil
       return "There is no discount to apply."
     else
       @total -= (@total * @discount / 100)
