8e1c4e6d0df6ac8083cfce71f8b39447baeaaa2d
Tracy Ho <tracyho0867@gmail.com>
2019-01-31 19:01:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,9 +15,7 @@ class CashRegister
       return "There is no discount to apply."
     else
       @total -= (@total * @discount / 100)
-    return "After the discount, the total comes to $#{@total}."
-
-
+      return "After the discount, the total comes to $#{@total}."
     end
 
 
